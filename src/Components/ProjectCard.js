3d4104cdb7ab6cf14313be4627b1d18635a5caed
rohitpd18 <rohitpd8101@gmail.com>
2023-08-16 13:32:50 +0530

    project details page added

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 import DefultImage from "../Assets/Image/defult-image.jpg";
+import { useNavigate } from "react-router-dom";
 
 const ProjectCard = (props) => {
+  let navigate = useNavigate()
   return (
-    <div className="project-body mx-auto mb-4" >
+    <div onClick={()=>{navigate("/project/"+props.id)}} className="project-body mx-auto mb-4" >
       <img
         className="project-image shado-lg rounded"
         src={DefultImage} data-aos="zoom-in" 
--- src/Components/[ProjectId].js
+++ src/Components/[ProjectId].js
@@ -1,117 +1,107 @@
 import React from "react";
 import DefultImage from "../Assets/Image/defult-image.jpg";
 import "../Assets/Css/ProjectPage.css";
-import EventCard from "./EventCard";
+import Loading from "./Loading";
+import MemberCard from "./MemberCard";
+import { useParams } from "react-router-dom";
+import axios from "../axios";
+import { useEffect, useState } from "react";
 
 const Projects = () => {
+  const params = useParams();
+  const id = params;
+  console.log(id);
+
+  const [project, setProject] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function fatchData() {
+    try {
+      setLoading(true);
+      const req = await axios.get("/allproject/" + id.id);
+      console.log(req.data.project);
+      setProject(req.data.project);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fatchData(); // eslint-disable-next-line
+  }, []);
+
   return (
     <>
-      {/* Project carousel */}
-      <div className="project-carousel">
-        <div
-          id="carouselExampleCaptions"
-          className="carousel slide"
-          data-bs-ride="false"
-        >
-          <div className="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
+      {!loading ? (
+        <div>
+          {/* carousel */}
+          <div className="project-img">
+            <img className="w-100" src={DefultImage} alt=".." />
           </div>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={DefultImage} className="d-block w-100" alt="..." />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>
-                  Some representative placeholder content for the first slide.
-                </p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img src={DefultImage} className="d-block w-100" alt="..." />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>
-                  Some representative placeholder content for the second slide.
-                </p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img src={DefultImage} className="d-block w-100" alt="..." />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>
-                  Some representative placeholder content for the third slide.
-                </p>
-              </div>
-            </div>
-          </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
 
-      {/* Project Team */}
-      <section className="upcoming-event mt-5">
-        <div className="text-center font-800 my-2" data-aos="zoom-in">
-          <h1 className="fw-bold fs-1">Project Name</h1>
+          {/* Project Details */}
+          <section className="mt-5">
+            <div
+              className="text-center font-800 my-4 mx-auto"
+              data-aos="zoom-in"
+            >
+              <h1 className="fw-bold fs-1">{project.name}</h1>
+            </div>
+            <div className="event-details-text">
+              {/* <h2>{project.topic} </h2> */}
+              {/* <h6 className="mb-4">{project.startDate}th May 2023, at {project.time} A.M</h6> */}
+              <p>
+                {project.description}
+                <img
+                  src={DefultImage}
+                  className="d-block w-75 my-4"
+                  alt="..."
+                />
+                <p />
+                laboriosam, aspernatur blanditiis nostrum sunt libero,
+                repudiandae consectetur expedita ad incidunt est molestiae quas.
+                Soluta, at. Et maxime eveniet natus error esse officiis impedit
+                fuga quisquam, maiores tenetur eum distinctio itaque dolores
+                deleniti a totam atque quidem tempore accusamus quia animi
+                suscipit. Rem libero, vero voluptas laudantium placeat eius cum
+                aperiam quos facilis, quod q quo fugiat molestiae ipsam eaque
+                corporis cupiditate vitae tempore qui illo minima voluptas,
+                commodi illum ducimus totam magni et beatae. Voluptatibus
+                officia recusandae quis, labore ut enim corrupti nobis sint?
+                Commodi qui aliquid eius alias dignissimos, in recusandae? Eum
+                illo ex ratione, iure molestiae et provident dolores odit quidem
+                quibusdam perspiciatis odio architecto! Officia repudiandae ea
+                distinctio. Deserunt laudantium rerum{" "}
+                <strong>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Voluptas quam cumque id repellendus animi facere quia dicta
+                  officiis voluptatibus mollitia.{" "}
+                </strong>{" "}
+                perferendis, eum, nulla fuga porro dolores dignissimos dolorum
+                laborum, pariatur libero eius animi possimus distinctio eveniet
+                iure facere dolorem. Quo consectetur deleniti ratione numquam
+                voluptas enim repellat pariatur? Suscipit accusamus qui corporis
+                velit harum accusantium veniam, delectus recusandae et repellat
+                minima totam, soluta sed modi deserunt consequuntur mollitia.
+                Maxime porro eius magni cumque, aliq alias et qui repellat labor
+                Officiis, rem. Amet ipsam placeat, quas ea obcaecati iusto quam
+                asperiores est enim aspernatur impedit facilis quibusdam id,
+                consequuntur natus quia sunt quos veritatis cupiditate mollitia!
+                Tempora delectus quae vitae magnam numquam repellat! Rerum,
+                fugiat iusto eaque voluptas maxime non.
+              </p>
+                <div className="text-center fw-bold fs-1 mt-2"> Team Member </div>
+              <div className="d-flex justify-content-center gap-2 flex-wrap">
+                  <MemberCard />
+                  <MemberCard />
+                  <MemberCard />
+              </div>
+            </div>
+          </section>
         </div>
-        <div className="container text-center mt-5">
-          <div className="d-flex flex-row flex-wrap justify-content-center">
-            <EventCard />
-            <EventCard />
-            <EventCard />
-          </div>
-        </div>
-      </section>
-
-      {/* Project Details */}
-      <section className="">
-        <div className="text-center font-800 my-4 mx-auto" data-aos="zoom-in">
-          <h1 className="fw-bold fs-1">Project Details</h1>
-        </div>
-        <div className="project-details-text" data-aos="fade-up"></div>
-      </section>
+      ) : (
+        <Loading />
+      )}
     </>
   );
 };
